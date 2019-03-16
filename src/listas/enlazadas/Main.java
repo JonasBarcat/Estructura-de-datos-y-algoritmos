093ac14a8ce802f9cdb187b505aa559baa51a32a
jonas <jonas@192.168.1.100>
2019-03-15 21:34:04 -0300

    Listas Enlazadas: punto 1 completo

--- src/listas/enlazadas/Main.java
+++ src/listas/enlazadas/Main.java
@@ -13,15 +13,52 @@ public class Main {
         public static void main(String[] args){
         Tren t;
     
-        t=new Tren();
+        t=new Tren();//iniciar una lista vacía METODO 1 (el resto en la clase tren)
         
-        t.agregarAlInicio("Rojo");
+        //agrego unos cuantos nodos al inicio
+        t.agregarAlInicio("Rojo");   // USO DEL METODO 3
         t.agregarAlInicio("Verde");
         t.agregarAlInicio("Amarillo");
         t.agregarAlInicio("Azul");
+        t.agregarAlInicio("Marron");
+        t.agregarAlInicio("Negro");
+        t.agregarAlInicio("Blanco");
+        t.agregarAlInicio("Vintage");
         
-        t.listaVacia();
-        t.mostrarTren();
+        //Verifico si la lista esta vacia o no
+        t.estaListaVacia();     // USO DE METODO 2
+        t.recorrerNodos();      // USO DEL METODO 5
+        
+//        llamada a metodo agregar nodo al final
+        t.agregarAlFinal("Violeta");  // USO DEL METODO 4
+        t.recorrerNodos();      // USO DEL METODO 5
+        
+        //veo la cantidad de nodos que hay
+        t.cantidadNodos();      // USO DEL METODO 6
        
+        
+        t.eliminarPrimero(); //USO DEL METODO 7
+        t.recorrerNodos();
+        
+        
+        t.eliminarUltimo(); //USO DEL METODO 8
+        t.recorrerNodos();
+        
+        t.dadoUnValorEliminar(3);  // USO DEL METODO 9
+        t.recorrerNodos();
+        
+        
+        t.mostrarContenido(2); // USO DEL METODO 10
+        
+        t.modificarNodoI(2, "Frutilla");
+        t.recorrerNodos();
+        
+        
+        t.insertarNodo(3, "Purpura");  //USO DEL METODO 12
+        t.recorrerNodos();
+        
+        
+        t.eliminarNodo(3);
+        t.recorrerNodos();
     }
 }
